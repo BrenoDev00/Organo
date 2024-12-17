93cb05a5a5c81706f09c5e1b6da22e99d0b19cd2
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-12-17 16:20:20 -0300

    feat: add Banner component

--- organo/src/App.jsx
+++ organo/src/App.jsx
@@ -1,14 +1,10 @@
+import { Banner } from "./components/Banner";
+
 function App() {
   return (
-    <>
-      <div className="bg-blue-300">
-        <h1 className="text-4xl font-bold underline">Olá, mundo!</h1>
-
-        <div className="text-center">
-          <p className="text-2xl text-gray-200 underline">lorem ipsum dolor sit amet</p>
-        </div>
-      </div>
-    </>
+    <div>
+      <Banner />
+    </div>
   );
 }
 
---
+++ organo/src/components/Banner/index.jsx
@@ -0,0 +1,11 @@
+export const Banner = () => {
+  return (
+    <header className="bg-bg-blue">
+      <img
+        className="w-full"
+        src="/images/banner.png"
+        alt="Banner de fundo do organo."
+      />
+    </header>
+  );
+};
