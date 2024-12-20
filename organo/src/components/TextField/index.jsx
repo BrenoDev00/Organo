12ee57b7410b2a10e821bf7f516ebf0a237b383d
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-12-20 10:06:12 -0300

    feat: update TextField component

--- organo/src/components/TextField/index.jsx
+++ organo/src/components/TextField/index.jsx
@@ -1,12 +1,7 @@
 export const TextField = (props) => {
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col gap-[8px]">
-      <label className="text-[18px] font-semibold">{props.label}</label>
-=======
     <div className="flex flex-col gap-[8px] font-montserrat">
       <label className="text-[16px] font-semibold">{props.label}</label>
->>>>>>> develop
       <input
         className="py-[20px] px-[15px] focus:outline-none shadow-md"
         type={props.type}
