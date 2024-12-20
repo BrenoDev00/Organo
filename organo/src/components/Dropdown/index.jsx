22b844c138ff9aeb383ab7d1e747e2179454d84d
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-12-20 10:04:52 -0300

    feat: update Dropdown component

--- organo/src/components/Dropdown/index.jsx
+++ organo/src/components/Dropdown/index.jsx
@@ -1,11 +1,5 @@
 export const Dropdown = (props) => {
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col gap-[8px]">
-      <label className="font-semibold text-[18px]">{props.label}</label>
-
-      <select className="py-[20px] px-[15px] focus:outline-none shadow-md">
-=======
     <div className="flex flex-col gap-[8px] font-montserrat">
       <label className="font-semibold text-[16px]">{props.label}</label>
 
@@ -15,7 +9,6 @@ export const Dropdown = (props) => {
         value={props.value}
         onChange={(event) => props.onChange(event.target.value)}
       >
->>>>>>> develop
         <option value="">Escolha um time</option>
 
         {props.teams.map((team) => {
