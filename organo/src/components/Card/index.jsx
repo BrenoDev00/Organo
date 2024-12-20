4fdb5a15b3ea7b06a687463b0526a4bfd56537a4
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-12-20 09:58:25 -0300

    feat: add Card component

--- organo/src/components/Card/index.jsx
+++ organo/src/components/Card/index.jsx
@@ -1,7 +1,10 @@
-export const Card = () => {
+export const Card = (props) => {
   return (
     <article className="flex flex-col w-[262px] shadow-lg rounded-[10px]">
-      <div className="flex justify-center h-[90px] bg-blue-500 w-full rounded-tl-[10px] rounded-tr-[10px]">
+      <div
+        className="flex justify-center h-[90px] bg-blue-500 w-full rounded-tl-[10px] rounded-tr-[10px]"
+        style={{ backgroundColor: props.backgroundColor }}
+      >
         <img
           className="translate-y-1/4 w-[120px] h-[120px] rounded-full "
           src="https://github.com/BrenoDev00.png"
@@ -10,7 +13,9 @@ export const Card = () => {
       </div>
 
       <div className="flex flex-col gap-[10px] bg-white font-montserrat text-center rounded-bl-[10px] rounded-br-[10px] px-[30px] py-[27px] ">
-        <h3 className="text-bg-blue text-[18px] font-semibold mt-[50px]">Breno</h3>
+        <h3 className="text-bg-blue text-[18px] font-semibold mt-[50px]">
+          Breno
+        </h3>
         <p>Desenvolvedor Front-End</p>
       </div>
     </article>
