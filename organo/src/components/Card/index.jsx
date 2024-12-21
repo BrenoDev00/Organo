565b5ec260a56cd85a08f55ee4466fcf63af5a60
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-12-20 22:22:02 -0300

    fix: style adjustment in the card component

--- organo/src/components/Card/index.jsx
+++ organo/src/components/Card/index.jsx
@@ -1,6 +1,6 @@
 export const Card = (props) => {
   return (
-    <article className="flex flex-col w-[262px] shadow-lg rounded-[10px]">
+    <article className="flex flex-col w-[262px] shadow-lg rounded-[10px] bg-white">
       <div
         className="flex justify-center h-[90px] w-full rounded-tl-[10px] rounded-tr-[10px]"
         style={{ backgroundColor: props.backgroundColor }}
@@ -13,10 +13,10 @@ export const Card = (props) => {
       </div>
 
       <div className="flex flex-col gap-[10px] bg-white font-montserrat text-center rounded-bl-[10px] rounded-br-[10px] px-[30px] py-[27px] ">
-        <h3 className="text-bg-blue text-[18px] font-semibold mt-[50px]">
+        <h3 className="text-bg-blue text-[18px] font-semibold mt-[50px] break-words">
           {props.name}
         </h3>
-        <p>{props.position}</p>
+        <p className="break-words">{props.position}</p>
       </div>
     </article>
   );
