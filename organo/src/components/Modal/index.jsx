dc209f9180ad5ca62317d4e4eee2089c74733d1b
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-12-31 19:16:20 -0300

    feat: add Modal component

--- organo/src/components/Modal/index.jsx
+++ organo/src/components/Modal/index.jsx
@@ -2,7 +2,10 @@ import { Button } from "../Button";
 
 export const Modal = (props) => {
   return (
-    <dialog className="hidden absolute top-1/2 translate-y-1/2  bg-light-gray w-[500px] flex flex-col gap-[80px] items-center px-[100px] py-[50px] rounded-[20px] shadow-xl">
+    <dialog
+      open={props.isOpen}
+      className="absolute top-1/2 translate-y-1/2  bg-light-gray w-[500px] flex flex-col gap-[80px] items-center px-[100px] py-[50px] rounded-[20px] shadow-xl"
+    >
       <div className="flex flex-col items-center gap-[50px]">
         <div className="rounded-full border-2 border-bg-blue flex justify-center items-center p-[20px] bg-white">
           {props.icon}
@@ -11,8 +14,12 @@ export const Modal = (props) => {
       </div>
       {props.children}
       <div className="flex gap-[60px]">
-        <Button title={props.primaryButtonMessage} variant="default" />
-        <Button title={props.secondaryButtonMessage} />
+        <Button
+          type="button"
+          title={props.primaryButtonMessage}
+          variant="default"
+        />
+        <Button type="button" title={props.secondaryButtonMessage} />
       </div>
     </dialog>
   );
