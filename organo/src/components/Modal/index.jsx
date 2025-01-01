bac0b8fa8640d95d56148da2b2afe8ab15a98f8c
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-12-31 22:58:49 -0300

    implement logic to open modal

--- organo/src/components/Modal/index.jsx
+++ organo/src/components/Modal/index.jsx
@@ -3,8 +3,9 @@ import { Button } from "../Button";
 export const Modal = (props) => {
   return (
     <dialog
-      open={props.isOpen}
-      className="absolute top-1/2 translate-y-1/2  bg-light-gray w-[500px] flex flex-col gap-[80px] items-center px-[100px] py-[50px] rounded-[20px] shadow-xl"
+      className={`${
+        props.isOpen ? "flex" : "hidden"
+      } bg-light-gray w-[500px] flex-col gap-[80px] items-center px-[100px] py-[50px] rounded-[20px] shadow-xl`}
     >
       <div className="flex flex-col items-center gap-[50px]">
         <div className="rounded-full border-2 border-bg-blue flex justify-center items-center p-[20px] bg-white">
@@ -14,12 +15,12 @@ export const Modal = (props) => {
       </div>
       {props.children}
       <div className="flex gap-[60px]">
+        <Button type="button" title={props.primaryButtonMessage} />
         <Button
           type="button"
-          title={props.primaryButtonMessage}
-          variant="default"
+          title={props.secondaryButtonMessage}
+          variant="secondary"
         />
-        <Button type="button" title={props.secondaryButtonMessage} />
       </div>
     </dialog>
   );
