1ec62aa6f539c9a69f8a05b79ebddbdb6241bc35
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-01-07 19:59:10 -0300

    refactor: refactor modal component

--- organo/src/components/Modal/index.jsx
+++ organo/src/components/Modal/index.jsx
@@ -5,9 +5,9 @@ export const Modal = (props) => {
     <div
       className={`${
         props.isOpen
-          ? "opacity-100 pointer-events-auto flex"
-          : "opacity-0 pointer-events-none"
-      } z-10 bg-transparent backdrop-blur-md h-screen w-screen fixed top-0 bottom-0 left-0 right-0 justify-center items-center transition-opacity duration-300`}
+          ? "opacity-100 pointer-events-auto flex duration-300"
+          : "opacity-0 pointer-events-none duration-100"
+      } z-10 bg-transparent backdrop-blur-md h-screen w-screen fixed top-0 bottom-0 left-0 right-0 justify-center items-center transition-opacity `}
     >
       <dialog
         className={`bg-light-gray w-[500px] flex flex-col gap-[80px] items-center px-[100px] py-[50px] rounded-[20px] shadow-xl`}
